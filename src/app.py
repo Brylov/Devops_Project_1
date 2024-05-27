2d0ebf582124eac0fc982c57d5be6207e9b5c743
brylo <guybrylov@gmail.com>
2024-05-27 09:11:10 +0300

    added DB, updated req, save word function

--- src/app.py
+++ src/app.py
@@ -1,9 +1,21 @@
 from flask import Flask, request, jsonify, render_template, send_file
 from deep_translator import GoogleTranslator
 from gtts import gTTS
+from pymongo import MongoClient
+from dotenv import load_dotenv
 import os
 
+# Load environment variables from .env file
+load_dotenv()
+
 app = Flask(__name__)
+
+# Retrieve MongoDB connection details from environment variables
+mongodb_uri = os.getenv('MONGODB_URI')
+
+# Initialize MongoDB client
+client = MongoClient(mongodb_uri)
+db = client.get_database()
 
 @app.route('/')
 def index():
@@ -20,6 +32,18 @@ def translate():
     translated_text = translator.translate(english_text)
     
     return jsonify({'translated_text': translated_text})
+
+@app.route('/saveword', methods=['POST'])
+def saveword():
+    data = request.get_json()
+    english_text = data.get('english_text')
+    translated_text = data.get('translated_text')
+    
+    if not english_text or not translated_text:
+        return jsonify({'error': 'Both English and translated text are required.'}), 400
+    
+    db.TranslatorHistory.insert_one({'english_text': english_text, 'translated_text': translated_text})
+    return jsonify({'success': True})
 
 @app.route('/tts', methods=['POST'])
 def tts():
@@ -46,6 +70,7 @@ def tts():
 @app.route('/get_tts')
 def get_tts():
     return send_file('translated_text.mp3', mimetype='audio/mpeg')
+
 
 
 if __name__ == '__main__':
