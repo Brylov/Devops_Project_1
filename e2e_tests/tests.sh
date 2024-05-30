bd9dc72aa0980ce55b63059b043412e224c8d79d
brylo <guybrylov@gmail.com>
2024-05-30 05:29:10 +0300

    fix e2e tests jq not supported -- used awk instead

--- e2e_tests/tests.sh
+++ e2e_tests/tests.sh
@@ -4,7 +4,7 @@ set -e
 
 # URL of your frontend and backend
 FRONTEND_URL="http://localhost"
-BACKEND_URL="http://localhost:5000"
+BACKEND_URL="http://localhost:5000/"
 
 # Test 1: Check if the frontend is up
 response=$(curl --write-out "%{http_code}\n" --silent --output /dev/null $FRONTEND_URL)
@@ -23,12 +23,16 @@ if [ "$response" -ne 200 ]; then
 fi
 echo "Backend is reachable."
 
-# Test 3: Perform a translation request
-translation_response=$(curl -X POST -H "Content-Type: application/json" -d '{"text": "hello", "inputLang": "English", "outputLang": "Japanese"}' "$BACKEND_URL/translate")
-if [[ "$translation_response" != *"こんにちは"* ]]; then
+
+translation_response=$(curl -s -X POST -H "Content-Type: application/json" -d '{"text": "hello", "inputLang": "en", "outputLang": "ja"}' "$BACKEND_URL/translate")
+translated_text=$(echo $translation_response | grep -oP '"translated_text":"\\K[^"]+')
+
+if [ "$translated_text" != "こんにちは" ]; then
   echo "Translation test failed. Response: $translation_response"
   exit 1
 fi
 echo "Translation test passed."
 
 # Add more tests as needed
+
+echo "E2E tests completed successfully."
