7274cb0efab60e14308d934229bb696c0224d8f2
brylo <guybrylov@gmail.com>
2024-06-02 13:41:37 +0300

    entrypoint for back change hardcode url to dynamic from env

--- frontend.entrypoint.sh
+++ frontend.entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Replace placeholders with environment variables
-sed -i 's|apiUrl\s*=\s*"[^"]*"|apiUrl="http://backend:5000/api"|g' /usr/share/nginx/html/main.*.js
+sed -i "s|apiUrl\s*=\s*\"[^\"]*\"|apiUrl=\"$API_URL\"|g" /usr/share/nginx/html/main.*.js
 
 # Start Nginx
 nginx -g 'daemon off;'
