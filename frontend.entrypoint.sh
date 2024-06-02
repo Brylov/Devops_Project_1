762b2277d4b7dc04585bafbe579606bed232579f
brylo <guybrylov@gmail.com>
2024-06-02 11:12:14 +0300

    dynamic api uri in frontend

--- frontend.entrypoint.sh
+++ frontend.entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Replace placeholders with environment variables
-sed -i 's|API_URL_PLACEHOLDER|'${API_URL}'|g' /usr/share/nginx/html/main.*.js
+sed -i 's|apiUrl\s*=\s*"[^"]*"|apiUrl="backend:5000"|g' /usr/share/nginx/html/main.*.js
 
 # Start Nginx
 nginx -g 'daemon off;'
